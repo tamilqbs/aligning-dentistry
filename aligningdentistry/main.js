a256e06a8e96c119c4fedbce392973b5e1e3d267
tamilqbs <151618569+tamilqbs@users.noreply.github.com>
2024-01-24 18:39:55 +0530

    Add files via upload

---
+++ aligningdentistry/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.navbar').addClass('sticky-top');
+        } else {
+            $('.navbar').removeClass('sticky-top');
+        }
+    });
+
+
+})(jQuery);
+
+
+
